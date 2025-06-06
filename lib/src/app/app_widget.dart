bb4f1d75eaee595e5485861493ff6f211454750a
Alexandre Pinto <alexandre.pinto@proway.treina>
2025-06-06 11:38:30 -0300

    commit

---
+++ lib/src/app/app_widget.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+import 'package:utilidades/src/app/app_routes.dart';
+
+class AppWidgets extends StatelessWidget {
+  const AppWidgets({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "App Utilidades",
+      initialRoute: "/home",
+      routes: generateRoutes(),
+    );
+  }
+}
